384ef582913fb1aef7277bcb544db8a1f7b50f12
Kin Lane <kinlane@gmail.com>
2013-03-20 15:40:46 -0700

    row header and footers

--- assets/themes/twitter/js/provider.js
+++ assets/themes/twitter/js/provider.js
@@ -2,23 +2,34 @@ function listProviders()
     {
     newscount = 0;
     $.getJSON('data/providers.json', function(data) {
+    	
     	$toggle=1;
+    	
+    	rowheader = '<td align="center" valign="top">';
+    	$('#providerListing').append(rowheader);	
+    	
     	 $.each(data['stack'], function(key, val) {
+    	 	
 				var template = $('#providerListingTemplate').html();
 				var html = Mustache.to_html(template, val);
 				$('#providerListing').append(html);
+				
 				if($toggle==2){ 
-					html = "</tr><tr>";
-					$('#providerListing').append(html);
+					rows = "</tr><tr>";
+					$('#providerListing').append(rows);
 					$toggle=1;
 					}
 				else{
+					rows = '</td><td align="center" valign="top">';
+					$('#providerListing').append(rows);					
 					$toggle++;
 				}
-				alert($toggle);
 	        });
         
         });
+        
+    	rowfooter = '</td></tr>';
+    	$('#providerListing').append(rowfooter);	        
         
     }    
      
