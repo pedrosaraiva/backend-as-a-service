43954eff59b314d0c9eee164392f01e44a8af027
Kin Lane <kinlane@gmail.com>
2013-03-20 15:56:41 -0700

    fuck cells

--- assets/themes/twitter/js/provider.js
+++ assets/themes/twitter/js/provider.js
@@ -1,21 +1,10 @@
 function listProviders()
     {
-    newscount = 0;
     $.getJSON('data/providers.json', function(data) {
-    	$toggle=0;
     	 $.each(data['stack'], function(key, val) {
-				if($toggle==0){ 
-					var template = $('#providerListingTemplate').html();
-					var html = Mustache.to_html(template, val);
-					$('#providerListing').append('<tr>'+html);			
-					$toggle=1;
-					}	
-				else{
-					var template = $('#providerListingTemplate').html();
-					var html = Mustache.to_html(template, val);
-					$('#providerListing').append(html+'</tr>');
-					$toggle=0;						
-					}
+			var template = $('#providerListingTemplate').html();
+			var html = Mustache.to_html(template, val);
+			$('#providerListing').append(html);
 	        });
         });
 
