370f58af4732d9489a68337772780490ef0d7d65
Kin Lane <kinlane@gmail.com>
2013-03-20 15:31:06 -0700

    2 column

--- assets/themes/twitter/js/provider.js
+++ assets/themes/twitter/js/provider.js
@@ -2,11 +2,20 @@ function listProviders()
     {
     newscount = 0;
     $.getJSON('data/providers.json', function(data) {
-    	
+    	$toggle=1;
     	 $.each(data['stack'], function(key, val) {
 				var template = $('#providerListingTemplate').html();
 				var html = Mustache.to_html(template, val);
 				$('#providerListing').append(html);
+				if($toggle==2){ 
+					html = "</tr><tr>";
+					$('#providerListing').append(html);
+					$toggle=1;
+					}
+				else{
+					$toggle=2;
+				}
+				
 	        });
         
         });
