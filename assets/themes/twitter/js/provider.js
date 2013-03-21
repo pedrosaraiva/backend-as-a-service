1cf8b5ef7ab6689410ead18a3e1e2fd295eae148
Kin Lane <kinlane@gmail.com>
2013-03-20 20:53:15 -0700

    back to tables :(

--- assets/themes/twitter/js/provider.js
+++ assets/themes/twitter/js/provider.js
@@ -1,10 +1,19 @@
 function listProviders()
     {
     $.getJSON('data/providers.json', function(data) {
+    	toggle = 0;
     	 $.each(data['stack'], function(key, val) {
 			var template = $('#providerListingTemplate').html();
 			var html = Mustache.to_html(template, val);
-			$('#providerListing').append(html);
+			$('#providerListing').append(html);    	 	
+    	 	
+    	 	if(toggle==1){
+				$('#providerListing').append('</tr><tr>');
+				toggle=0;
+				}
+			else{
+				toggle=1;
+			}
 	        });
         });
 
