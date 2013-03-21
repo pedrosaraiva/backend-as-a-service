9ae5bfae3f6f65565b6679f645d72fdbda324ca9
Kin Lane <kinlane@gmail.com>
2013-03-20 21:13:55 -0700

    back to lists and find a way to force height

--- assets/themes/twitter/js/provider.js
+++ assets/themes/twitter/js/provider.js
@@ -3,22 +3,9 @@ function listProviders()
     $.getJSON('data/providers.json', function(data) {
     	toggle = 0;
     	 $.each(data['stack'], function(key, val) {
-    	 	if(toggle==1){
-				alert("1");
-				var template = $('#providerListingTemplate1').html();
-				var html = Mustache.to_html(template, val);
-				$('#providerListing').append(html);    					
-					
-				toggle=0;
-				}
-			else{
-				alert("2");
-				var template = $('#providerListingTemplate2').html();
-				var html = Mustache.to_html(template, val);
-				$('#providerListing').append(html);    						
-				
-				toggle=1;
-			}
+			var template = $('#providerListingTemplate').html();
+			var html = Mustache.to_html(template, val);
+			$('#providerListing').append(html);    						
 	        });
         });
 
