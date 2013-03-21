e0b28c89a0efb1f1672582bf924a1f4d9712ed99
Kin Lane <kinlane@gmail.com>
2013-03-20 21:03:47 -0700

    try different approach

--- assets/themes/twitter/js/provider.js
+++ assets/themes/twitter/js/provider.js
@@ -3,16 +3,22 @@ function listProviders()
     $.getJSON('data/providers.json', function(data) {
     	toggle = 0;
     	 $.each(data['stack'], function(key, val) {
-    	 	alert(toggle);
-			var template = $('#providerListingTemplate').html();
-			var html = Mustache.to_html(template, val);
-			$('#providerListing').append(html);    	 	
+    	 	alert(toggle); 	
     	 	
     	 	if(toggle==1){
-				$('#providerListing').append('</tr><tr>');
+					
+				var template = $('#providerListingTemplate1').html();
+				var html = Mustache.to_html(template, val);
+				$('#providerListing').append(html);    					
+					
 				toggle=0;
 				}
 			else{
+				
+				var template = $('#providerListingTemplate2').html();
+				var html = Mustache.to_html(template, val);
+				$('#providerListing').append(html);    						
+				
 				toggle=1;
 			}
 			alert($('#providerListing').html());
