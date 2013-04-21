eefbdf16bcdcb7363dfc9ad58ba5c041c3b52ec3
Kin Lane <kinlane@gmail.com>
2013-04-21 12:08:41 -0700

    making img disappear

--- assets/themes/twitter/js/company.js
+++ assets/themes/twitter/js/company.js
@@ -24,19 +24,19 @@ function getCompanyDetails(id)
                 $('#companyDetail').append(html);
                 
 	            website = val['website'];
-	        	if(website!=''){ document.getElementById("home-icon").href=website; } else { document.getElementById("home-icon-img").display=='none'; }              
+	        	if(website!=''){ document.getElementById("home-icon").href=website; } else { document.getElementById("home-icon-img").style.display=='none'; }              
                 
 	            blog = val['blog'];
-	        	if(blog!=''){ document.getElementById("blog-icon").href=blog; } else { document.getElementById("home-blog-img").display=='none'; }
+	        	if(blog!=''){ document.getElementById("blog-icon").href=blog; } else { document.getElementById("home-blog-img").style.display=='none'; }
 	        	                
 	            blogrss = val['blogrss'];
-	        	if(blogrss!=''){ document.getElementById("blogrss-icon").href=blogrss; } else { document.getElementById("blogrss-icon-img").display=='none'; }
+	        	if(blogrss!=''){ document.getElementById("blogrss-icon").href=blogrss; } else { document.getElementById("blogrss-icon-img").style.display=='none'; }
 	        	
 	            twitter = val['twitter'];
-	        	if(twitter!=''){ document.getElementById("twitter-icon").href=twitter; } else { document.getElementById("twitter-icon-img").display=='none'; }
+	        	if(twitter!=''){ document.getElementById("twitter-icon").href=twitter; } else { document.getElementById("twitter-icon-img").style.display=='none'; }
 	        	
 	            github = val['github'];
-	        	if(github!=''){ document.getElementById("github-icon").href=github; } else { document.getElementById("github-icon-img").display=='none'; }	        		        	                
+	        	if(github!=''){ document.getElementById("github-icon").href=github; } else { document.getElementById("github-icon-img").style.display=='none'; }	        		        	                
             	}
           	});                                    
         });
