a1a6806582308ec807d252112bce20c867f297da
Kin Lane <kinlane@gmail.com>
2013-04-21 11:47:44 -0700

    adding website linkage

--- assets/themes/twitter/js/company.js
+++ assets/themes/twitter/js/company.js
@@ -13,13 +13,20 @@ function getCompanyDetails(id)
     {
     $.getJSON('data/companies.json', function(data) {  	
         $.each(data['company'], function(key, val) {
+        	
         	thisid = val['id'];
+        	
         	if(thisid == id)
         		{
                 var template = $('#companyDetailTemplate').html();
                 var html = Mustache.to_html(template, val);
                 $('#companyDetail').append(html);
             	}
+            
+            website = val['website'];
+        	if(thisid == id){ $("a#home-icon").attr('href',website); }
+        			            	
+            	
           	});                                    
         });
     }  
