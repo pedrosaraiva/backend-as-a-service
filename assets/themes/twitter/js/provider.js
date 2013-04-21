23d04799e3674e12eeaaa65a5688341b995e837d
kinlane <kinlane@gmail.com>
2013-04-21 03:32:30 -0400

    fix company listing

--- assets/themes/twitter/js/provider.js
+++ assets/themes/twitter/js/provider.js
@@ -2,7 +2,7 @@ function listProviders()
     {
     $.getJSON('data/companies.json', function(data) {
     	toggle = 0;
-    	 $.each(data['stack'], function(key, val) {
+    	 $.each(data['company'], function(key, val) {
 			var template = $('#providerListingTemplate').html();
 			var html = Mustache.to_html(template, val);
 			$('#providerListing').append(html);    						
