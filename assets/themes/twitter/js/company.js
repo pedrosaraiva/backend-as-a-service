bb8afc3c97601323e62c4395f34563b79aecc21d
kinlane <kinlane@gmail.com>
2013-04-21 12:55:06 -0400

    adding detail pages for companies

---
+++ assets/themes/twitter/js/company.js
@@ -0,0 +1,26 @@
+function listCompanies()
+    {
+    $.getJSON('data/companies.json', function(data) {
+    	toggle = 0;
+    	 $.each(data['company'], function(key, val) {
+			var template = $('#companyListingTemplate').html();
+			var html = Mustache.to_html(template, val);
+			$('#companyListing').append(html);    						
+	        });
+        });
+    }    
+     
+function getCompanyDetails(id)
+    {
+    $.getJSON('data/companies.json', function(data) {  	
+        $.each(data['company'], function(key, val) {
+        	thisid = new val['id'];
+        	if(thisid == id)
+        		{
+                var template = $('#companyDetailTemplate').html();
+                var html = Mustache.to_html(template, val);
+                $('#companyDetail').append(html);
+            	}
+          	});                                    
+        });
+    }  
